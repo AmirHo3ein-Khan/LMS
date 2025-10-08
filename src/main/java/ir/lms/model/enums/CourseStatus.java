0820dcaed24fa1014f7878b439b65dc403935b01
RBP <amirkhp1382@gmail.com>
2025-10-08 18:19:59 +0330

    update my CourseStatus

--- src/main/java/ir/lms/model/enums/CourseStatus.java
+++ src/main/java/ir/lms/model/enums/CourseStatus.java
@@ -2,19 +2,24 @@ package ir.lms.model.enums;
 
 import lombok.Getter;
 
-@Getter
 public enum CourseStatus {
- FILLED(1, "پرشده", "12343") ,
- UNFILLED(2,"تعریف نشده", "12345") ,;
-
- private final int value;
- private final String name;
- private final String code;
-
- CourseStatus(int value, String name, String code) {
-  this.value = value;
-  this.name = name;
-  this.code = code;
- }
-
+ FILLED , UNFILLED
 }
+
+
+//@Getter
+//public enum CourseStatus {
+// FILLED(1, "پرشده", "12343") ,
+// UNFILLED(2,"تعریف نشده", "12345") ,;
+//
+// private final int value;
+// private final String name;
+// private final String code;
+//
+// CourseStatus(int value, String name, String code) {
+//  this.value = value;
+//  this.name = name;
+//  this.code = code;
+// }
+//
+//}
