4bcab154390fd092de4381c30572703f698fd13d
RBP <amirkhp1382@gmail.com>
2025-10-15 11:54:01 +0330

    docs(AccountRepository): change AccountRepository
    Remove exist by username method.

--- src/main/java/ir/lms/repository/AccountRepository.java
+++ src/main/java/ir/lms/repository/AccountRepository.java
@@ -3,14 +3,11 @@ package ir.lms.repository;
 import ir.lms.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
     Optional<Account> findByUsername(String username);
-
     Optional<Account> findByAuthId(UUID authId);
-    boolean existsByUsername(String username);
 }
