c2fca466694a2c4c626125b29b2b8641db91225e
RBP <amirkhp1382@gmail.com>
2025-10-15 12:28:46 +0330

    feat(exception): Add CourseHasNotLimitException exception
    Add CourseHasNotLimitException exception as it is used in the application.

---
+++ src/main/java/ir/lms/exception/CourseHasNotLimitException.java
@@ -0,0 +1,7 @@
+package ir.lms.exception;
+
+public class CourseHasNotLimitException extends RuntimeException {
+    public CourseHasNotLimitException(String message) {
+        super(message);
+    }
+}
