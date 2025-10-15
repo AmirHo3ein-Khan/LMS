e9ceef7930a84ce938628774831168849f20df15
RBP <amirkhp1382@gmail.com>
2025-10-15 13:53:11 +0330

    refactor(repository): change query of QuestionRepository with named query

--- src/main/java/ir/lms/repository/QuestionRepository.java
+++ src/main/java/ir/lms/repository/QuestionRepository.java
@@ -2,21 +2,12 @@ package ir.lms.repository;
 
 import ir.lms.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question,Long> {
-
-    @Query("""
-       select q
-       from Question q
-       join q.examQuestions eq
-       where eq.exam.id = :examId
-       """)
-    List<Question> findQuestionsOfExam(@Param("examId") Long examId);
+    List<Question> findByExamQuestions_Exam_Id(Long examId);
 
 }
