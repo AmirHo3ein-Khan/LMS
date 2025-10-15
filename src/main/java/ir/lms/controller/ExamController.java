a7cb6416fa787e0bb682767cf8bcf244cb3a3bf5
RBP <amirkhp1382@gmail.com>
2025-10-15 12:46:50 +0330

    refactor(controller): Add methods to ExamController
    Add start exam , submit exam , submit answer api for ExamController

--- src/main/java/ir/lms/controller/ExamController.java
+++ src/main/java/ir/lms/controller/ExamController.java
@@ -1,17 +1,22 @@
 package ir.lms.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
+import ir.lms.model.Answer;
 import ir.lms.model.ExamTemplate;
+import ir.lms.model.Option;
+import ir.lms.service.AnswerService;
 import ir.lms.service.ExamService;
+import ir.lms.util.dto.AnswerDTO;
 import ir.lms.util.dto.ApiResponse;
 import ir.lms.util.dto.ApiResponseDTO;
 import ir.lms.util.dto.ExamDTO;
+import ir.lms.util.dto.mapper.AnswerMapper;
 import ir.lms.util.dto.mapper.ExamMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import java.security.Principal;
 import java.time.Instant;
 import java.util.List;
 
@@ -21,13 +26,18 @@ public class ExamController {
 
     private final ExamService examService;
     private final ExamMapper examMapper;
+    private final AnswerService answerService;
+    private final AnswerMapper answerMapper;
 
-    public ExamController(ExamService examService, ExamMapper examMapper) {
+    public ExamController(ExamService examService, ExamMapper examMapper, AnswerService answerService, AnswerMapper answerMapper) {
         this.examService = examService;
         this.examMapper = examMapper;
+        this.answerService = answerService;
+        this.answerMapper = answerMapper;
     }
 
     private static final String TEACHER = "hasRole('TEACHER')";
+    private static final String STUDENT = "hasRole('STUDENT')";
     private static final String TEACHER_OR_STUDENT = "hasRole('TEACHER') or hasRole('STUDENT')";
 
     @PreAuthorize(TEACHER)
@@ -106,6 +116,33 @@ public class ExamController {
                         .timestamp(Instant.now().toString())
                         .build()
         );
+    }
+
+    @PreAuthorize(STUDENT)
+    @PostMapping("/start-exam/{examId}")
+    public ResponseEntity<ApiResponseDTO> studentStartExam(@PathVariable Long examId, Principal principal) {
+        examService.startExam(examId, principal);
+        return ResponseEntity.ok(new ApiResponseDTO("Exam start success.", true));
+    }
+
+    @PreAuthorize(STUDENT)
+    @PostMapping("/submit-exam/{examId}")
+    public ResponseEntity<ApiResponseDTO> studentSubmitExam(@PathVariable Long examId, Principal principal) {
+        examService.submitExam(examId, principal);
+        return ResponseEntity.ok(new ApiResponseDTO("Exam submit success.", true));
+    }
+
+
+    @PreAuthorize(STUDENT)
+    @PostMapping("/submit-answer")
+    public ResponseEntity<ApiResponseDTO> submitAnswer(@Valid  @RequestBody AnswerDTO answerDTO , Principal principal) {
+        Answer answer = answerMapper.toEntity(answerDTO , principal);
+        Option option = new Option();
+        if (answerDTO.getOptionId() != null) {
+            option = answerService.findOptionById(answerDTO.getOptionId());
+        }
+        answerService.saveAnswer(answerDTO.getType() , answer, option, answerDTO.getAnswerText());
+        return ResponseEntity.ok(new ApiResponseDTO("Answer saved success.", true));
     }
 
 }
