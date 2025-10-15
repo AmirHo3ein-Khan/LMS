6048cc4270ed6be0126e5778bd9709a44e220fa1
RBP <amirkhp1382@gmail.com>
2025-10-15 13:35:51 +0330

    refactor(repository): refactor methods of PersonRepository
    Add find all major of person method and change the is student assigned to course method

--- src/main/java/ir/lms/repository/PersonRepository.java
+++ src/main/java/ir/lms/repository/PersonRepository.java
@@ -3,21 +3,14 @@ package ir.lms.repository;
 import ir.lms.model.Person;
 import ir.lms.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.Set;
+import java.util.List;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person,Long> {
-    boolean existsByNationalCode(String nationalCode);
-    boolean existsByPhoneNumber(String phoneNumber);
+public interface PersonRepository extends JpaRepository<Person,Long> , JpaSpecificationExecutor<Person> {
+    boolean existsByNationalCodeAndPhoneNumber(String nationalCode, String phoneNumber);
     boolean existsByRolesContains(Role role);
-    @Query("""
-                SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
-                FROM Person p
-                JOIN p.offeredCourses c
-                WHERE p.id = :studentId AND c.id = :courseId""")
-    boolean isStudentAssignedToCourse(@Param("studentId") Long studentId, @Param("courseId") Long courseId);
+    boolean existsByIdAndOfferedCourses_Id(Long studentId, Long courseId);
+    List<Person> findAllByMajor_MajorName(String majorName);
 }
