7beb6b0e845f6de9a47b4e25acab20b705246238
RBP <amirkhp1382@gmail.com>
2025-10-15 13:14:15 +0330

    refactor(repository): refactor MajorRepository
    remove exist by major method, no longer used in my application.

--- src/main/java/ir/lms/repository/MajorRepository.java
+++ src/main/java/ir/lms/repository/MajorRepository.java
@@ -2,14 +2,11 @@ package ir.lms.repository;
 
 import ir.lms.model.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface MajorRepository extends JpaRepository<Major,Long> {
-    boolean existsByMajorName(String majorName);
     Optional<Major> findByMajorName(String majorName);
 }
