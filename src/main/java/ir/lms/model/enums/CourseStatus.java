7e619678b85590357048a266c35852c550218889
RBP <amirkhp1382@gmail.com>
2025-10-15 12:35:06 +0330

    style(enum): remove my comments in CourseStatus

--- src/main/java/ir/lms/model/enums/CourseStatus.java
+++ src/main/java/ir/lms/model/enums/CourseStatus.java
@@ -1,25 +1,5 @@
 package ir.lms.model.enums;
 
-import lombok.Getter;
-
 public enum CourseStatus {
  FILLED , UNFILLED
 }
-
-
-//@Getter
-//public enum CourseStatus {
-// FILLED(1, "پرشده", "12343") ,
-// UNFILLED(2,"تعریف نشده", "12345") ,;
-//
-// private final int value;
-// private final String name;
-// private final String code;
-//
-// CourseStatus(int value, String name, String code) {
-//  this.value = value;
-//  this.name = name;
-//  this.code = code;
-// }
-//
-//}
