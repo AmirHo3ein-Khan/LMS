2388a04ab75f390788d22a1bc0d1ec2bccd6b07f
RBP <amirkhp1382@gmail.com>
2025-10-15 12:32:06 +0330

    refactor(repository): remove method from CourseRepository
    Remove exists by major and title method no longer used in my application.

--- src/main/java/ir/lms/repository/CourseRepository.java
+++ src/main/java/ir/lms/repository/CourseRepository.java
@@ -2,24 +2,11 @@ package ir.lms.repository;
 
 import ir.lms.model.Course;
 import ir.lms.model.Major;
-import jakarta.websocket.server.PathParam;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
-
-
-    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END " +
-            "FROM Course c " +
-            "WHERE c.title = :title AND c.major.id = :majorId")
-    boolean existsByMajorAndTitle(@Param("majorId") Long majorId, @Param("title") String title);
-
     Optional<Course> findByTitleAndMajor(String title, Major major);
 }
