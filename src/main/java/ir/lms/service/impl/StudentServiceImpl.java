7702fb20c430c62b3afd8b862fb300d04b804ef6
RBP <amirkhp1382@gmail.com>
2025-10-15 14:11:46 +0330

    refactor(service): add AcademicCalender to student take course method StudentServiceImpl

--- src/main/java/ir/lms/service/impl/StudentServiceImpl.java
+++ src/main/java/ir/lms/service/impl/StudentServiceImpl.java
@@ -1,8 +1,10 @@
 package ir.lms.service.impl;
 
 import ir.lms.exception.AccessDeniedException;
-import ir.lms.exception.CourseHasNotLimit;
+import ir.lms.exception.CourseHasNotLimitException;
+import ir.lms.exception.CourseRegisterDateException;
 import ir.lms.exception.EntityNotFoundException;
+import ir.lms.model.AcademicCalender;
 import ir.lms.model.Account;
 import ir.lms.model.OfferedCourse;
 import ir.lms.model.Person;
@@ -13,16 +15,19 @@ import ir.lms.service.StudentService;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Service
 public class StudentServiceImpl implements StudentService {
     private final static String NOT_FOUND = "%s not found!";
     private final static String NOT_ACCESS_TO_COURSE = "This course is not in your major to take!";
+    private final static String UNIT_LIMITED = "Could not take the course (Unit limited)!";
 
 
     private final PersonRepository personRepository;
     private final AccountRepository accountRepository;
-    private final OfferedCourseRepository  offeredCourseRepository;
+    private final OfferedCourseRepository offeredCourseRepository;
 
     public StudentServiceImpl(PersonRepository personRepository, AccountRepository accountRepository, OfferedCourseRepository offeredCourseRepository) {
         this.accountRepository = accountRepository;
@@ -34,16 +39,33 @@ public class StudentServiceImpl implements StudentService {
     public void studentTakeCourse(Long courseId, Principal principal) {
 
         Account account = accountRepository.findByUsername(principal.getName())
-                .orElseThrow(()->  new EntityNotFoundException(String.format(NOT_FOUND, "Account")));
+                .orElseThrow(() -> new EntityNotFoundException(String.format(NOT_FOUND, "Account")));
 
         Person person = account.getPerson();
 
         OfferedCourse offeredCourse = offeredCourseRepository.findById(courseId)
                 .orElseThrow(() -> new EntityNotFoundException(String.format(NOT_FOUND, "Course")));
 
+        AcademicCalender academicCalender = offeredCourse.getTerm().getAcademicCalender();
+
+        LocalDate now = LocalDate.now();
+
+        if (academicCalender.getCourseRegistrationStart().isBefore(now)) {
+            throw new CourseRegisterDateException("Course register date not started!");
+        }
+        if (academicCalender.getCourseRegistrationEnd().isAfter(now)) {
+            throw new CourseRegisterDateException("Course register date ended!");
+        }
+
+        if (person.getOfferedCourses() != null) {
+            Integer totalUnits = offeredCourseRepository.getTotalUnitsByPersonId(person.getId());
+            if (totalUnits + offeredCourse.getCourse().getUnit() <= 20) {
+                throw new IllegalArgumentException(UNIT_LIMITED);
+            }
+        }
         Integer capacity = offeredCourse.getCapacity();
         if (capacity <= 0) {
-            throw new CourseHasNotLimit("The course has no limit!");
+            throw new CourseHasNotLimitException("The course has no limit!");
         }
 
         if (!(offeredCourse.getTerm().getMajor() == person.getMajor())) {
@@ -52,10 +74,11 @@ public class StudentServiceImpl implements StudentService {
 
         offeredCourse.setCapacity(offeredCourse.getCapacity() - 1);
 
-        offeredCourse.getStudent().add(person);
+        offeredCourse.setStudents(new ArrayList<>());
+        offeredCourse.getStudents().add(person);
         offeredCourseRepository.save(offeredCourse);
 
-
+        person.setOfferedCourses(new ArrayList<>());
         person.getOfferedCourses().add(offeredCourse);
         personRepository.save(person);
 
