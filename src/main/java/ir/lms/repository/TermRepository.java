1e49c3eb24cce60c51795c2a8989fb1006e21386
RBP <amirkhp1382@gmail.com>
2025-10-15 13:53:49 +0330

    refactor(repository): add exist by semester and major in TermRepository

--- src/main/java/ir/lms/repository/TermRepository.java
+++ src/main/java/ir/lms/repository/TermRepository.java
@@ -1,9 +1,12 @@
 package ir.lms.repository;
 
+import ir.lms.model.Major;
 import ir.lms.model.Term;
+import ir.lms.model.enums.Semester;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TermRepository extends JpaRepository<Term,Long> {
+    boolean existsBySemesterAndMajor(Semester semester, Major major);
 }
