214d652d7b5fcbb72eafcc51977f196917cfab80
RBP <amirkhp1382@gmail.com>
2025-10-15 13:31:34 +0330

    refactor(service): Add tree apis  PersonController
    Add teacher register , manager register  and role to person apis to PersonController

--- src/main/java/ir/lms/controller/PersonController.java
+++ src/main/java/ir/lms/controller/PersonController.java
@@ -1,36 +1,88 @@
 package ir.lms.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
+import ir.lms.model.Person;
 import ir.lms.model.Role;
-import ir.lms.service.AuthService;
-import ir.lms.util.dto.ApiResponse;
-import ir.lms.util.dto.ApiResponseDTO;
-import ir.lms.util.dto.ChangeRoleRequestDTO;
-import ir.lms.util.dto.ResponseOfferedCourseDTO;
+import ir.lms.service.PersonService;
+import ir.lms.service.StudentService;
+import ir.lms.util.dto.*;
+import ir.lms.util.dto.mapper.PersonMapper;
+import ir.lms.util.dto.mapper.ProfileMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 import java.security.Principal;
 import java.time.Instant;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/person")
 public class PersonController {
-    private final AuthService authService;
+    private final PersonService personService;
+    private final ProfileMapper profileMapper;
+    private final PersonMapper personMapper;
+    private final StudentService studentService;
 
-    public PersonController(AuthService authService) {
-        this.authService = authService;
+
+    public PersonController(PersonService personService, ProfileMapper profileMapper,
+                            PersonMapper personMapper, StudentService studentService) {
+        this.personService = personService;
+        this.profileMapper = profileMapper;
+        this.personMapper = personMapper;
+        this.studentService = studentService;
     }
 
+    private static final String ADMIN = "hasRole('ADMIN')";
+    private static final String STUDENT = "hasRole('STUDENT')";
     private static final String ALL_AUTHENTICATED = "hasAnyRole('ADMIN','MANAGER','STUDENT','TEACHER','USER')";
+    private static final String ADMIN_OR_MANAGER = "hasAnyRole('ADMIN','MANAGER')";
+
+
+
+    @PreAuthorize(ADMIN_OR_MANAGER)
+    @PostMapping("/teacher-register")
+    public ResponseEntity<ApiResponse<PersonDTO>> teacherRegister(@Valid  @RequestBody PersonDTO request) {
+        Person person = personService.persist(personMapper.toEntity(request));
+        personService.addRoleToPerson("teacher" , person.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.<PersonDTO>builder()
+                        .success(true)
+                        .message("Register.success")
+                        .data(personMapper.toDto(person))
+                        .timestamp(Instant.now().toString())
+                        .build()
+        );
+    }
+
+    @PreAuthorize(ADMIN)
+    @PostMapping("/manager-register")
+    public ResponseEntity<ApiResponse<PersonDTO>> managerRegister(@Valid @RequestBody PersonDTO request) {
+        Person person = personService.persist(personMapper.toEntity(request));
+        personService.addRoleToPerson("manager" , person.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.<PersonDTO>builder()
+                        .success(true)
+                        .message("Register.success")
+                        .data(personMapper.toDto(person))
+                        .timestamp(Instant.now().toString())
+                        .build()
+        );
+    }
+
+    @PreAuthorize(ADMIN)
+    @PostMapping("/add/person-role")
+    public ResponseEntity<ApiResponseDTO> addRoleToPerson(@Valid @RequestBody AddRoleRequest request) {
+        personService.addRoleToPerson(request.getRole() , request.getPersonId());
+        return ResponseEntity.ok(new ApiResponseDTO("Add role success", true));
+    }
+
 
     @PreAuthorize(ALL_AUTHENTICATED)
     @PostMapping("/change-role")
-    public ResponseEntity<ApiResponseDTO> changeRole(@Valid  @RequestBody ChangeRoleRequestDTO request , Principal principal) {
-        authService.changeRole(principal.getName(), request.getRole());
+    public ResponseEntity<ApiResponseDTO> changeRole(@Valid @RequestBody ChangeRoleRequestDTO request, Principal principal) {
+        personService.changeRole(principal.getName(), request.getRole());
         return ResponseEntity.ok(new ApiResponseDTO("Change role success", true));
     }
 
@@ -40,8 +92,8 @@ public class PersonController {
         return ResponseEntity.status(HttpStatus.OK).body(
                 ApiResponse.<List<String>>builder()
                         .success(true)
-                        .message("courses.get.success")
-                        .data(authService.getPersonRoles(principal).stream()
+                        .message("roles.get.success")
+                        .data(personService.getPersonRoles(principal).stream()
                                 .map(Role::getName)
                                 .toList())
                         .timestamp(Instant.now().toString())
@@ -49,5 +101,32 @@ public class PersonController {
         );
     }
 
-    // todo : update profile and change pass
+    @PreAuthorize(ALL_AUTHENTICATED)
+    @PutMapping("/update-profile")
+    public ResponseEntity<ApiResponseDTO> updateProfile(@RequestBody UpdateProfileDTO dto, Principal principal) {
+        personService.updateProfile(profileMapper.toEntity(dto), principal);
+        return ResponseEntity.ok(new ApiResponseDTO("Update profile success", true));
+    }
+
+    @PreAuthorize(ADMIN)
+    @GetMapping("/search/{keyword}")
+    public ResponseEntity<ApiResponse<List<PersonDTO>>> search(@PathVariable String keyword) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponse.<List<PersonDTO>>builder()
+                        .success(true)
+                        .message("roles.get.success")
+                        .data(personService.search(keyword).stream()
+                                .map(personMapper::toDto)
+                                .toList())
+                        .timestamp(Instant.now().toString())
+                        .build()
+        );
+    }
+
+    @PreAuthorize(STUDENT)
+    @PostMapping("/take-course/{courseId}")
+    public ResponseEntity<ApiResponseDTO> studentGetCourse(@PathVariable Long courseId, Principal principal) {
+        studentService.studentTakeCourse(courseId, principal);
+        return ResponseEntity.ok(new ApiResponseDTO("Course get success.", true));
+    }
 }
