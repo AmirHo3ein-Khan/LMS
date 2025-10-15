5f369c1f3d67db1961d1eeb158f1afa92d6c262b
RBP <amirkhp1382@gmail.com>
2025-10-15 13:54:21 +0330

    refactor(service): add findTermCalenderByTermId in TermService

--- src/main/java/ir/lms/service/TermService.java
+++ src/main/java/ir/lms/service/TermService.java
@@ -1,7 +1,9 @@
 package ir.lms.service;
 
+import ir.lms.model.AcademicCalender;
 import ir.lms.model.Term;
 import ir.lms.service.base.BaseService;
 
 public interface TermService extends BaseService<Term, Long> {
+    AcademicCalender findTermCalenderByTermId(Long termId);
 }
